94f7ad1fff316d5528987cbe5bb8a1528e58f92c
Skylan Lew <skylanslew@gmail.com>
2022-09-09 09:35:23 -0700

    Add preliminary logic and UI functions fromUSD, printElements, printError, handleSumbission

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Convert from './js/convert.js'
+
+async function fromUSD(targetCode, amount){
+  const response = await Convert.fromUSD(targetCode, amount);
+  console.log(response);
+  if(response.result === "success"){
+    printElements(response, targetCode, amount);
+  } else {
+    printError(response, targetCode);
+  }
+}
+
+function printElements(response, targetCode, amount){
+  console.log("success!");
+}
+
+function printError(error, targetCode){
+  console.log("error!");
+}
+
+function handleFormSubmission(event){
+  event.preventDefault();
+  const targetCode = document.querySelector("#targetCode").value;
+  const amount = document.querySelector("#amount").value;
+  document.querySelector("#targetCode").value = null;
+  document.querySelector("#amount").value = null;
+  fromUSD(targetCode, amount);
+}
+
+document.querySelector('form').addEventListener("submit", handleFormSubmission);
